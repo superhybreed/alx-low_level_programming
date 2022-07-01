abf0fb564d1854bb2e457c8ae39c3dcb929666a8
superhybreed <davedivine09@gmail.com>
2022-06-30 18:24:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,23 @@
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: David Ugbabe
+ *
+ **/
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ *
+ **                puzzle, followed by a new line.
+ *
+ **
+ *
+ ** Return: Always 0.
+ *
+ **/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
